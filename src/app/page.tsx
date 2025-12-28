848d5d89185c292ee56c408fb4fb69a9784f1d8f
Mateo Morrison <24979743+mateomorrison@users.noreply.github.com>
2025-12-28 10:55:44 -0500

    change to font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         <section>
           {/* <h2 className="text-2xl font-bold mb-4">About Us</h2> */}
           <p className="mb-5 text-[1.1em]">
-            Morrison Jr. Group marks the continuation of a multi-generational family legacy originating in the Dominican Republic. Founded in 2025, it expands upon a journey that began with hands-on entrepreneurship and has evolved through the transformative core of financial technology, bridging traditional roots with global innovation.
+            <b>Morrison Jr. Group</b> marks the continuation of a multi-generational family legacy originating in the Dominican Republic. Founded in 2025, it expands upon a journey that began with hands-on entrepreneurship and has evolved through the transformative core of financial technology, bridging traditional roots with global innovation.
           </p>
           <div className="image-container" ref={imageRef}>
             <Image
